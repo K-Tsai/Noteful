8597e2e05b7478c7f65242376de5d1326ab97bbd
Kevin Tsai <k.tsai92@gmail.com>
2019-07-11 15:45:23 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <main className='App'>
-        {/* content goes here */}
+          <Route exact path='/' component={App}/>
       </main>
     );
   }
 }
 
 export default App;
+
